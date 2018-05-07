7121e1ebce9c894c0546ab9e9e6945e59d45fd9d
mashiguang0833@sina.com <mashiguang0833@sina.com>
2018-05-07 15:07:51 +0800

    sessionid, sessionPasswd

--- readme.md
+++ readme.md
@@ -4,3 +4,5 @@ install zookeeper
 
     docker run --name zk --restart always -d -p 0.0.0.0:2181:2181 -p 0.0.0.0:2888:2888 -p 0.0.0.0:3888:3888 zookeeper
 
+Conn2，复用sessionid, sessonPasswd的意义是什么？
+
---
+++ src/main/java/cn/niceabc/zk/conn/Conn2.java
@@ -0,0 +1,61 @@
+package cn.niceabc.zk.conn;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class Conn2 implements Watcher {
+
+    private static Logger log = LoggerFactory.getLogger(Conn2.class);
+
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        ZooKeeper zk = new ZooKeeper("192.168.199.211:2181",
+                5000,
+                new Conn2());
+        log.debug("zk state: {}", zk.getState());
+
+        try {
+            connectedSemaphore.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        log.debug("zk session established.");
+        long sessionid = zk.getSessionId();
+        byte[] pwd = zk.getSessionPasswd();
+
+        zk = new ZooKeeper("192.168.199.211:2181",
+                5000,
+                new Conn2(),
+                1L,             //illegal sessionid
+                "test".getBytes());       //illegal pwd
+        log.debug("zk state: {}", zk.getState());
+
+        zk = new ZooKeeper("192.168.199.211:2181",
+                5000,
+                new Conn2(),
+                sessionid,             //correct sessionid
+                pwd);                  //correct pwd
+        log.debug("zk state: {}", zk.getState());
+
+        //System.in.read();
+        TimeUnit.SECONDS.sleep(60);
+    }
+
+    public void process(WatchedEvent watchedEvent) {
+        log.debug("receive watched event: {}", watchedEvent);
+
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            connectedSemaphore.countDown();
+        }
+    }
+}
