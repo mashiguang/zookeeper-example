88fe9da2526774bcc11801c8e383c448185c3702
mashiguang0833@sina.com <mashiguang0833@sina.com>
2018-05-09 17:15:22 +0800

    watch

---
+++ src/main/java/cn/niceabc/zk/conn/ZnodeWatch.java
@@ -0,0 +1,57 @@
+package cn.niceabc.zk.conn;
+
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+public class ZnodeWatch {
+
+    private static Logger log = LoggerFactory.getLogger(ZnodeWatch.class);
+
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+
+        ZooKeeper zk = new ZooKeeper("192.168.199.211:2181",
+                500,
+                new Watcher() {
+                    public void process(WatchedEvent event) {
+                        log.debug("receive watched event: {}", event);
+
+                        if (Event.KeeperState.SyncConnected == event.getState()) {
+                            connectedSemaphore.countDown();
+                        }
+                    }
+                });
+        log.debug("zk state: {}", zk.getState());
+
+        try {
+            connectedSemaphore.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.debug("zk session established.");
+
+        Stat stat = zk.exists("/zk-test-persistent", new Watcher() {
+            public void process(WatchedEvent event) {
+                log.debug("event.state {}", event.getType());
+            }
+        });
+        if (stat != null)
+            zk.delete("/zk-test-persistent", stat.getVersion());
+
+        String path3 = zk.create("/zk-test-persistent",
+                "this is value".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.PERSISTENT);
+        log.debug("success create znode {}", path3);
+
+
+        System.in.read();
+    }
+
+}
