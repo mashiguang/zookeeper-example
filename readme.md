e8cd861ce857e4bb65645d5ed574fb23067dca41
mashiguang0833@sina.com <mashiguang0833@sina.com>
2018-05-09 11:33:45 +0800

    del

--- readme.md
+++ readme.md
@@ -6,3 +6,6 @@ install zookeeper
 
 Conn2，复用sessionid, sessonPasswd的意义是什么？
 
+del未成功，cn.niceabc.zk.conn.Znode2Del
+返回101代表什么?
+
---
+++ src/main/java/cn/niceabc/zk/conn/Znode2Del.java
@@ -0,0 +1,68 @@
+package cn.niceabc.zk.conn;
+
+import org.apache.zookeeper.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+public class Znode2Del implements Watcher {
+
+    private static Logger log = LoggerFactory.getLogger(Znode2Del.class);
+
+    private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+
+    public static void main(String[] args) throws IOException, KeeperException, InterruptedException {
+
+        ZooKeeper zk = new ZooKeeper("192.168.199.211:2181",
+                500,
+                new Znode2Del());
+        log.debug("zk state: {}", zk.getState());
+
+        try {
+            connectedSemaphore.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.debug("zk session established.");
+
+        zk.delete("/zk-test-ephemeral",
+                1,
+                new AsyncCallback.VoidCallback() {
+                    public void processResult(int i, String s, Object o) {
+                        log.debug("i: {}", i);
+                        log.debug("s: {}", s);
+                        log.debug("o: {}", o);
+                    }
+                },
+                "this is a context.");
+
+        /*zk.create("/zk-test-ephemeral",
+                "".getBytes(),
+                ZooDefs.Ids.OPEN_ACL_UNSAFE,
+                CreateMode.EPHEMERAL_SEQUENTIAL,
+                new AsyncCallback.StringCallback() {
+                    public void processResult(int i, String s, Object o, String s1) {
+                        log.debug("znode created. {}", s);
+                        log.debug("i: {}", i);
+                        log.debug("s: {}", s);
+                        log.debug("o: {}", o);
+                        log.debug("s1: {}", s1);
+                    }
+                },
+                "this is a context."
+        );*/
+        //log.debug("success create znode {}", path1);
+
+        System.in.read();
+    }
+
+    public void process(WatchedEvent watchedEvent) {
+        log.debug("receive watched event: {}", watchedEvent);
+
+        if (Event.KeeperState.SyncConnected == watchedEvent.getState()) {
+            connectedSemaphore.countDown();
+        }
+    }
+}
